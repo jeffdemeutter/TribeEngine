a62db9c4a5081276afba80d952c800d4d43f72df
Jeff De Meutter <jeffke.de.meutter@gmail.com>
2022-02-24 10:53:27 +0100

    Update TribeInstallPacks.bat

--- TribeInstallPacks.bat
+++ TribeInstallPacks.bat
@@ -1,19 +1,19 @@
 vcpkg install glm
-vcpkg install glm --triplet=x64-windows
+vcpkg install glm:x64-windows
 vcpkg install imgui
-vcpkg install imgui --triplet=x64-windows
+vcpkg install imgui:x64-windows
 vcpkg install imgui[opengl2-binding] --recurse
-vcpkg install imgui[opengl2-binding] --recurse --triplet=x64-windows
+vcpkg install imgui[opengl2-binding]:x64-windows --recurse
 vcpkg install imgui[sdl2-binding] --recurse
-vcpkg install imgui[sdl2-binding] --recurse --triplet=x64-windows
+vcpkg install imgui[sdl2-binding]:x64-windows --recurse
 vcpkg install opengl
-vcpkg install opengl --triplet=x64-windows
+vcpkg install opengl:x64-windows
 vcpkg install SDL2
-vcpkg install SDL2 --triplet=x64-windows
+vcpkg install SDL2:x64-windows
 vcpkg install SDL2-ttf
-vcpkg install SDL2-ttf --triplet=x64-windows
+vcpkg install SDL2-ttf:x64-windows
 vcpkg install SDL2-image
-vcpkg install SDL2-image --triplet=x64-windows
+vcpkg install SDL2-image:x64-windows
 vcpkg install sdl2-image[libjpeg-turbo] --recurse
-vcpkg install sdl2-image[libjpeg-turbo] --recurse --triplet=x64-windows
+vcpkg install sdl2-image[libjpeg-turbo]:x64-windows --recurse 
 pause
