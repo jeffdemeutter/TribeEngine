5746aa57c6c2c8616e7f78df645f36743ba7c2a7
Tboske <jeffke.de.meutter@gmail.com>
2022-05-02 01:02:50 +0200

    added service locator

--- Tribe/ServiceLocator.h
+++ Tribe/ServiceLocator.h
@@ -2,14 +2,10 @@
 #include "TribePCH.h"
 
 class SoundManager;
-
-class ServiceLocator
+class ServiceLocator final 
 {
 	static SoundManager* m_ServiceLocatorInstance;
 public:
 	static SoundManager* GetInstance() { return m_ServiceLocatorInstance; }
 	static void SetInstance(SoundManager* pSoundManager) { m_ServiceLocatorInstance = pSoundManager; }
-	
 };
-
-SoundManager* ServiceLocator::m_ServiceLocatorInstance{};
