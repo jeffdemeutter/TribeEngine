8b8804b3d38eb355f92633406d8ec989b53b8b1c
Tboske <jeffke.de.meutter@gmail.com>
2022-06-22 04:09:59 +0200

    added test for input

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,7 +1,19 @@
 #include "Game.h"
+#include "InputManager.h"
+#include "iostream"
 
+void PrintStuff(const std::string& s)
+{
+	std::cout << s << '\n';
+}
 
 void Game::LoadGame() const
 {
-	
+	InputManager::InputAction inputAction;
+	inputAction.ControllerID = 0;
+	inputAction.keyboardKey = SDL_SCANCODE_W;
+	inputAction.keyboardStroke = InputManager::Stroke::released;
+	inputAction.pCommand = new Command([] { PrintStuff("yay"); });
+
+	m_GameContext.pInput->AddInputMethod(inputAction);
 }
