2fda4a80f0a8dbc071bae03cd7a17832c16daa2a
Tboske <jeffke.de.meutter@gmail.com>
2022-06-22 04:06:57 +0200

    added singleton base class

---
+++ Tribe/Singleton.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template <typename T>
+class Singleton
+{
+public:
+	virtual ~Singleton() = default;
+
+protected:
+	static T& Instance()
+	{
+		static T instance{};
+		return instance;
+	}
+
+	Singleton() = default;
+	Singleton(const Singleton& other) = delete;
+	Singleton(Singleton&& other) = delete;
+	Singleton& operator=(const Singleton& other) = delete;
+	Singleton& operator=(Singleton&& other) = delete;
+};
