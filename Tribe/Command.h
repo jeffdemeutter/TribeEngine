b397a5e2b30f0a1fd60e87851edc45e3a727996d
Tboske <jeffke.de.meutter@gmail.com>
2022-06-22 04:06:41 +0200

    command class wrapper

---
+++ Tribe/Command.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <functional>
+
+class Command final
+{
+public:
+	Command(std::function<void()> func)
+		: m_Execute{ func }
+	{
+	}
+
+	~Command() = default;
+
+	void Execute() const {
+		m_Execute();
+	}
+
+private:
+	std::function<void()> m_Execute;
+};
