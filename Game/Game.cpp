e96c9a4143c0440e920ac89ad555434091a25014
Tboske <jeffke.de.meutter@gmail.com>
2022-07-14 15:15:55 +0200

    added test for fps

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
 #include "Font.h"
+#include "FpsComponent.h"
 #include "GameObject.h"
 #include "Scene.h"
 
@@ -9,18 +10,21 @@
 #include "ResourceManager.h"
 
 #include "RenderComponent.h"
+#include "TextComponent.h"
 #include "TransformComponent.h"
 
 void Game::LoadGame() const
 {
 	Font* pFont = ResourceManager::LoadFont("Lingua.otf", 36);
-	pFont;
+
 
 	const auto pScene = m_GameContext.pSceneManager->AddScene("MainScene");
 
 	const auto pGo = pScene->AddGameObject("TestObject");
 	const auto pTrans = pGo->AddComponent(new TransformComponent(pGo));
-	pGo->AddComponent(new RenderComponent(pGo, pTrans));
+	const auto pRender = pGo->AddComponent(new RenderComponent(pGo, pTrans));
+	const auto pText = pGo->AddComponent(new TextComponent(pGo, pRender, " ", pFont));
+	pGo->AddComponent(new FpsComponent(pGo, pText));
 	
 
 
