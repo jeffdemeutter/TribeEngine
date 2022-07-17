38f5688309c9da4e844e68d353731856ecf4662c
Tboske <jeffke.de.meutter@gmail.com>
2022-07-17 15:40:38 +0200

    added precompiled header

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,3 +1,4 @@
+#include "GamePCH.h"
 #include "Game.h"
 
 #include "Font.h"
@@ -6,29 +7,42 @@
 #include "Scene.h"
 
 #include "InputManager.h"
+#include "PlayerTankComponent.h"
 #include "SceneManager.h"
 #include "ResourceManager.h"
 
 #include "RenderComponent.h"
+#include "RenderManager.h"
 #include "TextComponent.h"
 #include "TransformComponent.h"
+#include "SpriteAnimationComponent.h"
 
 void Game::LoadGame() const
 {
 	Font* pFont = ResourceManager::LoadFont("Lingua.otf", 36);
+	pFont;
 
 	{ // main scene
-		const auto pScene = m_GameContext.pSceneManager->AddScene("MainScene");
+		const auto pScene = m_GameContext.pSceneManager->AddScene("SinglePlayer");
 
-		const auto pGo = pScene->AddGameObject("FpsObject");
-		const auto pTrans = pGo->AddComponent(new TransformComponent(pGo));
-		const auto pRender = pGo->AddComponent(new RenderComponent(pGo, pTrans));
-		const auto pText = pGo->AddComponent(new TextComponent(pGo, pRender, " ", pFont));
-		pGo->AddComponent(new FpsComponent(pGo, pText));
+		// tank
+		{ 
+			const auto pTank = pScene->AddGameObject("PlayerTank");
 
-		auto inputAction1 = InputAction(new Command([pGo] { pGo->ToggleActive(); }));
-		inputAction1.stroke = Stroke::released;
-		inputAction1.keyboardKey = SDL_SCANCODE_R;
-		m_GameContext.pInput->AddInputAction(inputAction1);
+			const auto pTransform = pTank->AddComponent(new TransformComponent(pTank, RenderManager::GetWindowCenter()));
+			const auto pRender = pTank->AddComponent(new RenderComponent(pTank, pTransform, "spritesheet.png"));
+			const auto pSAC = pTank->AddComponent(new SpriteAnimationComponent(pTank, pRender, 10));
+			pTank->AddComponent(new PlayerTankComponent(pTank, pTransform, pSAC));
+		}
+
+		// fps object
+		{
+			auto pFps = pScene->AddGameObject("fps");
+			
+			const auto pTransform = pFps->AddComponent(new TransformComponent(pFps));
+			const auto pRender = pFps->AddComponent(new RenderComponent(pFps, pTransform));
+			const auto pText = pFps->AddComponent(new TextComponent(pFps, pRender, "", pFont, { 255,255,0,255 }));
+			pFps->AddComponent(new FpsComponent(pFps, pText));
+		}
 	}
 }
---
+++ Game/GamePCH.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Structs.h"
+
+#pragma warning(push, 0)
+#include <glm/glm.hpp>
+#include <SDL2/SDL.h>
+#pragma warning (pop)
